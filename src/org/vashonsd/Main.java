d0eeb660760741f8a3a8ad1dbb8f6101a06a4f5f
gallegos.archer <galleos.archer@vashonsd.org>
2021-10-06 10:42:22 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,11 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(squareOf(10));
+    }
+    public static int squareOf(int x) {
+        return x * x;
+    }
+}
